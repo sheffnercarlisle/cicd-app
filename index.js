47a311364fec75a5619f004391770f75bdc59481
Scott Heffner <sheffner@carlisle.k12.ma.us>
2020-06-30 12:46:56 -0400

    Enjoying it

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Welcome to the CI/CD testing app!'))
+app.get('/', (req, res) => res.send('Welcome to the CI/CD testing app! Enjoy!'))
 
 app.listen(port)
